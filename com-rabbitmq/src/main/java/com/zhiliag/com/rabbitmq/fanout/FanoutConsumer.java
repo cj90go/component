6694385cb6606e97f3a3b4ac3e73971d5d586fff
lizhi05 <lizhi05@ehuatai.com>
2020-09-13 16:58:12 +0800

    优化代码问题

--- com-rabbitmq/src/main/java/com/zhiliag/com/rabbitmq/fanout/FanoutConsumer.java
+++ com-rabbitmq/src/main/java/com/zhiliag/com/rabbitmq/fanout/FanoutConsumer.java
@@ -22,21 +22,21 @@ public class FanoutConsumer {
     @Autowired
     ObjectMapper objectMapper;
 
-    @RabbitListener(queues = "${spring.rabbitmq.queue.fanout-name1}",containerFactory = "singleListenerContainer")
+    @RabbitListener(queues = "${spring.rabbitmq.queue.fanout-name1}")
     public void consumerFanoutOne(@Payload byte[] bytes){
         try{
-//            EventInfo eventInfo=objectMapper.readValue(bytes,EventInfo.class);
-            logger.info("消息模型-fanoutExchange-消费端1-接收到的消息为:{}",bytes);
+            EventInfo eventInfo=objectMapper.readValue(bytes,EventInfo.class);
+            logger.info("消息模型-fanoutExchange-消费端1-接收到的消息为:{}",eventInfo);
         }catch (Exception e){
             logger.error("消费模型-fanoutExchange-消费端1-接受到的消息异常",e.fillInStackTrace());
         }
     }
 
-    @RabbitListener(queues = "${spring.rabbitmq.queue.fanout-name2}",containerFactory = "singleListenerContainer")
+    @RabbitListener(queues = "${spring.rabbitmq.queue.fanout-name2}")
     public void consumerFanoutTwo(@Payload byte[] bytes){
         try{
-//            EventInfo eventInfo=objectMapper.readValue(bytes,EventInfo.class);
-            logger.info("消息模型-fanoutExchange-消费端1-接收到的消息为:{}",bytes);
+            EventInfo eventInfo=objectMapper.readValue(bytes,EventInfo.class);
+            logger.info("消息模型-fanoutExchange-消费端1-接收到的消息为:{}",eventInfo);
         }catch (Exception e){
             logger.error("消费模型-fanoutExchange-消费端2-接受到的消息异常",e.fillInStackTrace());
         }
